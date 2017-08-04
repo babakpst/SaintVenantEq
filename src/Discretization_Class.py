ff4053c14a653d0027019cf3a7cd6fc21749493f
Babak Poursartip <babakp@utexas.edu>
2017-08-04 09:13:07 -0500

    Add files via upload

--- src/Discretization_Class.py
+++ src/Discretization_Class.py
@@ -42,30 +42,40 @@ class Discretization:
 
     self.Length_Cell  = np.zeros( self.N_Cells, dtype=np.float ) # Stores the length of each cell
     self.Z_Cell       = np.zeros( self.N_Cells, dtype=np.float ) # Stores bottom elevation of each cell
-    self.Manning_Cell = np.zeros( self.N_Cells, dtype=np.float) # Stores the Manning's number of each cell
-    self.Width_Cell   = np.zeros( self.N_Cells, dtype=np.float) # Stores the Manning's number of each cell
+    self.Manning_Cell = np.zeros( self.N_Cells, dtype=np.float ) # Stores the Manning's number of each cell
+    self.Width_Cell   = np.zeros( self.N_Cells, dtype=np.float ) # Stores the Manning's number of each cell
+    self.X_Disc       = np.zeros( self.N_Cells, dtype=np.float ) # Stores the Manning's number of each cell
 
     Cell_Counter = 0
 
     for ii in range(Temp_No_reaches):
       CntrlVolume_Length = round(self.Experiment.Reach_Length[ii]/self.Experiment.Reach_Disc[ii],5)  # Control volume length, rounded to 5 decimal points. The length of the final cell in reach would be adjusted to fix the discretization, if necessary. 
-      Height       = Max_Height
+      Height             = Max_Height
 
       Z_loss             =  round(CntrlVolume_Length * self.Experiment.Reach_Slope[ii],8)
       Height            += round(0.5  * Z_loss,8)
       Total_Length       = 0
 
+      X_distance = 0.5 * CntrlVolume_Length
+      for jj in range(ii):
+        X_distance  += self.Experiment.Reach_Length[ii]
+
       for jj in range( self.Experiment.Reach_Disc[ii] - 1 ):
         self.Length_Cell[Cell_Counter]  = CntrlVolume_Length
-        Total_Length              += CntrlVolume_Length
-        Height                    -= Z_loss
+        self.X_Disc[Cell_Counter]       = X_distance
+        X_distance                     += CntrlVolume_Length
+        Total_Length                   += CntrlVolume_Length
+        Height                         -= Z_loss
 
         self.Z_Cell[Cell_Counter]       = Height
         self.Manning_Cell[Cell_Counter] = self.Experiment.Reach_Manning[ii]
         self.Width_Cell[Cell_Counter]   = self.Experiment.Reach_Width[ii]
         Cell_Counter += 1
+
       # The last cell: we need to separate the last cell in each reach to adjust the numerical error in the total length of the reach
       self.Length_Cell[Cell_Counter]  = self.Experiment.Reach_Length[ii] - Total_Length
+      X_distance                      = X_distance - 0.5 * CntrlVolume_Length + 0.5 * self.Length_Cell[Cell_Counter]
+      self.X_Disc[Cell_Counter]       =  X_distance  
       Height                         -= ( 0.5*Z_loss + 0.5 * self.Length_Cell[Cell_Counter] * self.Experiment.Reach_Slope[ii] )
 
       self.Z_Cell[Cell_Counter]       = Height
@@ -73,11 +83,11 @@ class Discretization:
       Cell_Counter += 1
       Max_Height   -= self.Experiment.Reach_Length[ii] * self.Experiment.Reach_Slope[ii]
 
-      self.Q_Up       = self.Experiment.Q_Up
-      self.V_in       = self.Experiment.V_in
-      self.Total_Time = self.Experiment.Total_Time
-      self.Time_Step  = self.Experiment.Time_Step
-      self.h_dw       = self.Experiment.h_dw
+    self.Q_Up       = self.Experiment.Q_Up
+    self.V_in       = self.Experiment.V_in
+    self.Total_Time = self.Experiment.Total_Time
+    self.Time_Step  = self.Experiment.Time_Step
+    self.h_dw       = self.Experiment.h_dw
   
     if self.N_Cells != Cell_Counter:
       sys.exit("FATAL ERROR: Mismatch between the number of cells! Check the Discretization_Class.")
@@ -87,5 +97,3 @@ class Discretization:
 
 
 # Check on the discretization: if ..._Cell = 0 => ERROR
-
-      
--- src/Solver_Class.py
+++ src/Solver_Class.py
@@ -17,8 +17,10 @@ class Solver:
     # -- Import libs/classes
     import numpy as np
     import Initialization_Class
+    import Visualization_Class
 
     Gravity = 9.81
+    Draw = Visualization_Class.Visualization()
 
     print(" Solving the DE ...")
 
@@ -53,6 +55,16 @@ class Solver:
     U_0  = np.zeros( N_Cells, dtype=np.float64 )
     E_F_0= np.zeros( N_Cells, dtype=np.float64 )
 
+    V_1   = np.zeros( N_Cells, dtype=np.float64 )
+    Q_1   = np.zeros( N_Cells, dtype=np.float64 )
+    Eta_1 = np.zeros( N_Cells, dtype=np.float64 )
+    E_1   = np.zeros( N_Cells, dtype=np.float64 )
+    A_1   = np.zeros( N_Cells, dtype=np.float64 )
+    l_P_1 = np.zeros( N_Cells, dtype=np.float64 )
+    R_h_1 = np.zeros( N_Cells, dtype=np.float64 )
+    F_1   = np.zeros( N_Cells, dtype=np.float64 )
+    C_1   = np.zeros( N_Cells, dtype=np.float64 )
+
     #
     A_F   = np.zeros( N_Cells+1, dtype=np.float64 )
     Eta_F = np.zeros( N_Cells+1, dtype=np.float64 )
@@ -60,11 +72,10 @@ class Solver:
     E_F   = np.zeros( N_Cells+1, dtype=np.float64 )
     Q_F   = np.zeros( N_Cells+1, dtype=np.float64 )
 
-    k_1V  = np.zeros( N_Cells, dtype=np.float64 ) # <modify> maybe just k_v and k_q so that it can be used for k_2 as well.
-    k_1Q  = np.zeros( N_Cells, dtype=np.float64 )
-
-    V_1   = np.zeros( N_Cells, dtype=np.float64 )
-    Q_1   = np.zeros( N_Cells, dtype=np.float64 )
+    k_1V  = np.zeros( N_Cells, dtype=np.float64 ) # <modify> remove this array
+    k_1Q  = np.zeros( N_Cells, dtype=np.float64 ) # <modify> remove this array
+    k_2V  = np.zeros( N_Cells, dtype=np.float64 ) # <modify> remove this array
+    k_2Q  = np.zeros( N_Cells, dtype=np.float64 ) # <modify> remove this array
 
     # Initialization 
     Q[:] = Ex.Q[:]
@@ -73,6 +84,7 @@ class Solver:
     Z[:] = Ex.Z[:]
     M[:] = Ex.M[:]
     B[:] = Ex.B[:]
+    X[:] = Ex.X[:]
 
     for nn in range(N_Steps):
       print(" Time step: %d out of %d " % (nn, N_Steps))
@@ -140,45 +152,51 @@ class Solver:
         Q_1[ii]   = Q[ii] + 0.5* k_1Q[ii]  # <modify> We really don't need to define k_1v and k_1q, just for the clarity of the code.
 
       for ii in range(N_Cells):  # These are the variables at {n+1}
-        A[ii]   = V_1[ii] / L[ii]
-        U[ii]   = Q_1[ii] / A[ii]
-        Eta[ii] = A[ii] / B[ii] + Z[ii]
-        E[ii]   = ((U[ii])**2) /2 +  Gravity*Eta[ii]
-        l_P[ii] = B[ii] + 2 * Eta[ii]
-        R_h[ii] = A[ii] / l_P[ii]
-        C[ii]   = ((Eta[ii])**2) / ((R_h[ii])**(4/3))
-        F[ii]   = Gravity * C[ii] * V[ii] * ((U[ii])**2)
+        A_1[ii]   = V_1[ii] / L[ii]
+        U_1[ii]   = Q_1[ii] / A_1[ii]
+        Eta_1[ii] = A[ii] / B[ii] + Z[ii]
+        E_1[ii]   = ((U_1[ii])**2) /2 +  Gravity*Eta_1[ii]
+        l_P_1[ii] = B[ii] + 2 * Eta_1[ii]  # <modify>
+        R_h_1[ii] = A_1[ii] / l_P_1[ii] # <modify>
+        C_1[ii]   = ((Eta_1[ii])**2) / ((R_h_1[ii])**(4/3)) # <modify>
+        F_1[ii]   = Gravity * C_1[ii] * V_1[ii] * ((U_1[ii])**2)
 
       # 
       for ii in range(N_Cells+1):
         if ii==0: # Boundary condition at face 1/2
-            A_F[ii]   = A[ii]
-            Eta_F[ii] = Eta[ii] + L[ii] * (( Z[ii] - Z[ii+1]) / (L[ii] + L[ii+1]))
-            U_F[ii]   = Ex.Q_Up / A_F[ii]
-            E_F[ii]   = ((U_F[ii])**2)/2 + Gravity * Eta_F[ii]
+          A_F[ii]   = A[ii]
+          Eta_F[ii] = Eta[ii] + L[ii] * (( Z[ii] - Z[ii+1]) / (L[ii] + L[ii+1]))
+          U_F[ii]   = Ex.Q_Up / A_F[ii]
+          E_F[ii]   = ((U_F[ii])**2)/2 + Gravity * Eta_F[ii]
         elif ii != 1 and ii != N_Cells: # middle cells
-            A_F[ii]   = (L[ii+1]*A[ii]   + L[ii]*A[ii+1]   )/( L[ii+1] + L[ii] )
-            Eta_F[ii] = (L[ii+1]*Eta[ii] + L[ii]*Eta[ii+1] )/( L[ii+1] + L[ii] )
-            # Temp: E_{i+1/2}^{n+1/2}
-            Temp   = ( 1.0 / (V_1[ii]+V_1[ii+1]) ) * ( E_F_0[ii] *( V_0[ii] + V_0[ii+1] ) - E[ii]*V[ii] - E[ii+1]*V[ii+1] + E_0[ii]*V_0[ii] + E_0[ii+1]*V_0[ii+1] + DT * ( Q[ii] * E[ii] - Q[ii+1] * E[ii+1] - (1.0/2.0) * ( U[ii] * F[ii] + U[ii+1] * F[ii+1]  ) +      Q_0[ii] * E_0[ii] - Q_0[ii+1] * E_0[ii+1] - (1.0/2.0) * ( U_0[ii] * F_0[ii] + U_0[ii+1] * F_0[ii+1]  )   ) )
-
-            U_F[ii]   = (2*(Temp - Gravity * Eta_F[ii] ) )**(0.5)
-            Q_F[ii]   = A_F[ii] * U_F[ii]          
+          A_F[ii]   = (L[ii+1]*A[ii]   + L[ii]*A[ii+1]   )/( L[ii+1] + L[ii] )
+          Eta_F[ii] = (L[ii+1]*Eta[ii] + L[ii]*Eta[ii+1] )/( L[ii+1] + L[ii] )
+          # Temp: E_{i+1/2}^{n+1/2}
+          Temp   = ( 1.0 / (V_1[ii]+V_1[ii+1]) ) * ( E_F[ii] *( V[ii] + V[ii+1] ) - E_1[ii]*V_1[ii] - E_1[ii+1]*V_1[ii+1] + E[ii]*V[ii] + E[ii+1]*V[ii+1] + DT * ( Q_1[ii] * E_1[ii] - Q_1[ii+1] * E_1[ii+1] - (1.0/2.0) * ( U_1[ii] * F_1[ii] + U_1[ii+1] * F_1[ii+1]  ) + Q[ii] * E[ii] - Q[ii+1] * E[ii+1] - (1.0/2.0) * ( U[ii] * F[ii] + U[ii+1] * F[ii+1]  )   ) )
+          U_F[ii]   = ( 2*(Temp - Gravity * Eta_F[ii] ) )**(0.5)
+          Q_F[ii]   = A_F[ii] * U_F[ii]          
         elif ii == N_Cells: # Boundary condition at face N+1/2
-          pass
+          Delta_Z   = L[ii-1] * ( ( Z[ii-2]-Z[ii-1] )/( L[ii-2]+L[ii-1] )  )
+          Z_N1      = Z[ii-1] - Delta_Z
+          Eta_F[ii] = h_dw
+          A_F[ii]   = (Eta_F[ii] - Z_N1 ) * B[ii-1]
+          Q_F[ii]   = (1.0/M[ii-1]) * A[ii-1] * ((R_h[ii-1])**(2.0/3.0))  * ( ( (Eta[ii-1]-Eta_F[ii])/(0.5*L[ii-1])  )**(1.0/2.0) )
+          U_F[ii]   = Q_F[ii] / A_F[ii]
+          E_F[ii]   = ((U_F[ii])**2)/2 + Gravity * Eta_F[ii]
 
-      for ii in range(N_Cells+1): # To find k2 in the Runge-Kutta method and find the solution at n + 1
-        V_0  = V
-        Q_0  = Q
-        E_0  = E
-        F_0  = F
-        U_0  = U
+      for ii in range(N_Cells): # To find k2 in the Runge-Kutta method and find the solution at n + 1
+        V_0[ii]  = V[ii]
+        Q_0[ii]  = Q[ii]
+        E_0[ii]  = E[ii]
+        F_0[ii]  = F[ii]
+        U_0[ii]  = U[ii]
+        E_F_0[ii]= E_F[ii]
 
-        k_2V =
-        k_2Q =
-        V = V + k_2V
-        Q = Q + k_2Q
+        k_2V[ii]  = DT * ( Q_F[ii] - Q_F[ii+1] )  # <modify> remove
+        k_2Q[ii]  = (DT / L[ii]) * ( Q_F[ii] * U_F[ii] - Q_F[ii+1] * U_F[ii+1] + Gravity * A_F[ii]* Eta_F[ii] - Gravity * A_F[ii+1]* Eta_F[ii+1] -F[ii] ) # <modify> remove
+        V[ii] = V[ii] + k_2V[ii]
+        Q[ii] = Q[ii] + k_2Q[ii]
 
-      # Visualization
+      Draw.Plot(N_Cells, X, Eta, "Water elevation")
     # End loop on the time steps
 
