3b7d1ab7482471749fdf811277fc23dd082e0464
Babak Poursartip <babakp@utexas.edu>
2017-09-01 09:10:43 -0500

    Add files via upload

--- src/Solver_Class.py
+++ src/Solver_Class.py
@@ -21,7 +21,6 @@ class Solver:
         import Initialization_Class
         import Visualization_Class
 
-
         Gravity = 9.81
         Draw = Visualization_Class.Visualization()
 
@@ -45,6 +44,7 @@ class Solver:
         Z_F  = np.zeros(N_Cells*2+1, dtype=np.float64 )
         M    = np.zeros(N_Cells, dtype=np.float64 )
         B    = np.zeros(N_Cells, dtype=np.float64 )
+        Fr   = np.zeros(N_Cells, dtype=np.float64 )
 
         A    = np.zeros(N_Cells, dtype=np.float64 )
         U    = np.zeros(N_Cells, dtype=np.float64 )
@@ -105,22 +105,37 @@ class Solver:
         X[:] = Ex.X[:]
         X_F[:] = Ex.X_F[:]
 
+        slowness = 50000
+        h_upstream = V[0]/(B[0]*L[0])
+
         print(" Time marching ... ")
         for nn in range(N_Steps):
             print(" Time step: %d out of %d " % (nn, N_Steps))
- 
+
+            if nn < slowness:
+              Q_Upstream = Ex.Q_Up * (nn/ float(slowness))
+            else:
+              Q_Upstream = Ex.Q_Up        
+        
             for ii in range(N_Cells):
                 A[ii]   = V[ii] / L[ii]
                 U[ii]   = Q[ii] / A[ii]
                 Eta[ii] = A[ii] / B[ii] + Z[ii]
                 E[ii]   = ((U[ii])**2.0) /(float(2)) +  Gravity*Eta[ii]
-                l_P[ii] = B[ii] + 2 * Eta[ii]
+                l_P[ii] = B[ii] + 2.0 * (Eta[ii]-Z[ii])
                 R_h[ii] = A[ii] / l_P[ii]
-                C[ii]   = ((M[ii])**2) / ((R_h[ii])**(4.0/3.0))
-                F[ii]   = Gravity * C[ii] * V[ii] * ((U[ii])**2)
+                C[ii]   = ((M[ii])**2.0) / ((R_h[ii])**(4.0/3.0))
+                F[ii]   = Gravity * C[ii] * V[ii] * ((U[ii])**2.0)
+                Fr[ii]  = U[ii]/((Gravity * (Eta[ii] - Z[ii]) )**(0.5))
+                if Fr[ii] >= 1.0:
+                  print("Flow is not subcritical")
+                  check = input(" Error: press ENTER to exit ")
+                  sys.exit()
+
 
             # <delete>
-            if (nn%100) == 0:
+            if (nn%10000) == 0:
+                print(Q_Upstream)
                 RealTime = round(nn*DT,5)
                 TITLE = " at time: " + str(RealTime)
                 Draw.Plot_at_Cell(N_Cells, X, Z, Q, V, Eta, U, E, A, TITLE)
@@ -143,11 +158,18 @@ class Solver:
             if nn == 0:
                 for ii in range(N_Cells+1):
                     if ii==0: # Boundary condition at face 1/2
-                        Q_F[ii]   = Ex.Q_Up
-                        A_F[ii]   = A[ii] # This means A_(1/2) = A_1
-                        U_F[ii]   = Q_F[ii] / A_F[ii]
+                        Q_F[ii]   = Q_Upstream
+                        #A_F[ii]   = A[ii] # This means A_(1/2) = A_1
+                        #U_F[ii]   = Q_F[ii] / A_F[ii]
+                        #Eta_F[ii] = Eta[ii] + L[ii] * (( Z[ii] - Z[ii+1]) / (L[ii] + L[ii+1]))
+
+                        Z_0 = Z[ii] + L[ii] * (( Z[ii] - Z[ii+1]) / (L[ii] + L[ii+1]))
+                        #Eta_F[ii] = h_upstream+ Z_0
                         Eta_F[ii] = Eta[ii] + L[ii] * (( Z[ii] - Z[ii+1]) / (L[ii] + L[ii+1]))
-                        E_F[ii]   = ((U_F[ii])**2)/2 + Gravity * Eta_F[ii]
+                        A_F[ii]   =  (Eta_F[ii] - Z_0)* B[ii]
+
+                        U_F[ii]   = Q_F[ii] / A_F[ii]
+                        E_F[ii]   = ((U_F[ii])**2.0)/2.0 + Gravity * Eta_F[ii]
                     elif ii != 0 and ii != N_Cells: # middle cells - The subtraction is due to the fact that Python numbering is starting from 0
                         A_F[ii]   = (L[ii]*  A[ii-1] + L[ii-1]*  A[ii] )/( L[ii] + L[ii-1] )
                         Eta_F[ii] = (L[ii]*Eta[ii-1] + L[ii-1]*Eta[ii] )/( L[ii] + L[ii-1] )
@@ -173,18 +195,31 @@ class Solver:
                         #Q_F[ii]   = (1.0/M[ii-1]) * A_F[ii] * ((R_h[ii-1])**(2.0/3.0))  * ( ( (Eta[ii-1]-Eta_F[ii])/(0.5*L[ii-1])  )**(1.0/2.0) ) # <modify>
                         Q_F[ii]   = Q[ii-1]
                         U_F[ii]   = Q_F[ii] / A_F[ii]
-                        E_F[ii]   = ((U_F[ii])**2)/2 + Gravity * Eta_F[ii]
+                        E_F[ii]   = ((U_F[ii])**2.0)/2.0 + Gravity * Eta_F[ii]
             elif nn != 0:   
                 for ii in range(N_Cells+1):
                     if ii==0: # Boundary condition at face 1/2
-                        Q_F[ii]   = Ex.Q_Up
-                        A_F[ii]   = A[ii]
-                        U_F[ii]   = Q_F[ii] / A_F[ii]
+                        Q_F[ii]   = Q_Upstream
+                        #A_F[ii]   = A[ii]
+                        #U_F[ii]   = Q_F[ii] / A_F[ii]
+                        #Eta_F[ii] = Eta[ii] + L[ii] * (( Z[ii] - Z[ii+1]) / (L[ii] + L[ii+1]))
+
+                        Z_0 = Z[ii] + L[ii] * (( Z[ii] - Z[ii+1]) / (L[ii] + L[ii+1]))
+                        #Eta_F[ii] = h_upstream+ Z_0
                         Eta_F[ii] = Eta[ii] + L[ii] * (( Z[ii] - Z[ii+1]) / (L[ii] + L[ii+1]))
-                        E_F[ii]   = ((U_F[ii])**2)/2 + Gravity * Eta_F[ii]
+                        A_F[ii]   =  (Eta_F[ii] - Z_0)* B[ii]
+
+                        U_F[ii]   = Q_F[ii] / A_F[ii]
+                        E_F[ii]   = ((U_F[ii])**2.0)/2.0 + Gravity * Eta_F[ii]
                     elif ii != 0 and ii != N_Cells: # middle cells
-                        A_F[ii]   = (L[ii]*  A[ii-1] + L[ii-1]*  A[ii] )/( L[ii] + L[ii-1] )
+                        A_F[ii]   = ( L[ii]*  A[ii-1] + L[ii-1]*  A[ii] )/( L[ii] + L[ii-1] )
+                        
                         Eta_F[ii] = ( L[ii]*Eta[ii-1] + L[ii-1]*Eta[ii] ) / ( L[ii] + L[ii-1] )
+
+                        #if Eta_F[ii] != A_F[ii] / B[ii]  + Z_F[2*ii]:
+                        #    print(" Fatal Error: inconsistency btw A and eta: %d %d %f %f"% (nn, ii, Eta_F[ii], A_F[ii] / B[ii]  + Z_F[2*ii] ))
+                        #    check = input(" Error: press ENTER to exit ")
+                        #    sys.exit()
                         E_F[ii]   = ( 1.0 / (V[ii-1]+V[ii]) ) * ( E_F_0[ii] *( V_0[ii-1] + V_0[ii] ) - E[ii-1]*V[ii-1] - E[ii]*V[ii] + E_0[ii-1]*V_0[ii-1] + E_0[ii]*V_0[ii] + DT * ( Q[ii-1] * E[ii-1] - Q[ii] * E[ii] - (1.0/2.0) * ( U[ii-1] * F[ii-1] + U[ii] * F[ii]  ) + Q_0[ii-1] * E_0[ii-1] - Q_0[ii] * E_0[ii] - (1.0/2.0) * ( U_0[ii-1] * F_0[ii-1] + U_0[ii] * F_0[ii]  )   ) )
                         if E_F[ii]  < 0:
                             print(" Fatal Error: Negative Energy: %d, %d, %f " % (nn, ii, E_F[ii] ))
@@ -197,7 +232,7 @@ class Solver:
                             print(" %30.20f" % (E_F_1[ii] - Gravity * Eta_F_1[ii]))                            
                             check = input(" Error: press ENTER to exit ")
                             sys.exit()
-                        U_F[ii]   = (2*(E_F[ii] - Gravity * Eta_F[ii] ) )**(0.5)
+                        U_F[ii]   = (2.0*(E_F[ii] - Gravity * Eta_F[ii] ) )**(0.5)
                         Q_F[ii]   = A_F[ii] * U_F[ii]
                     elif ii == N_Cells: # Boundary condition at face N+1/2
                         Delta_Z   = L[ii-1] * ( ( Z[ii-2]-Z[ii-1] )/( L[ii-2]+L[ii-1] )  )
@@ -211,13 +246,13 @@ class Solver:
                         #Q_F[ii]   = (1.0/M[ii-1]) * A_F[ii] * ((R_h[ii-1])**(2.0/3.0))  * ( ( (Eta[ii-1]-Eta_F[ii])/(0.5*L[ii-1])  )**(1.0/2.0) )
                         Q_F[ii]   = Q[ii-1]
                         U_F[ii]   = Q_F[ii] / A_F[ii]
-                        E_F[ii]   = ((U_F[ii])**2)/2 + Gravity * Eta_F[ii]
+                        E_F[ii]   = ((U_F[ii])**2.0)/2.0 + Gravity * Eta_F[ii]
 
             # <delete>
-            if (nn%100) == 0:
-              RealTime = round(nn*DT,5)
-              TITLE = "k-1 at time: " + str(RealTime)
-              Draw.Plot_Full(2,N_Cells, X_F, Z_F, Q, Q_F, Eta, Eta_F, U, U_F, E, E_F, A, A_F, TITLE)
+            if (nn%10000) == 0:
+                RealTime = round(nn*DT,5)
+                TITLE = "k-1 at time: " + str(RealTime)
+                Draw.Plot_Full(2,N_Cells, X_F, Z_F, Q, Q_F, Eta, Eta_F, U, U_F, E, E_F, A, A_F, TITLE)
 
             for ii in range(N_Cells): # To find k1 in the Runge-Kutta method and find the solution at n+1/2
                 k_1V[ii]  =  DT * ( Q_F[ii] - Q_F[ii+1] )  # <modify> remove
@@ -226,26 +261,51 @@ class Solver:
                 V_1[ii]   = V[ii] + 0.5* k_1V[ii]
                 Q_1[ii]   = Q[ii] + 0.5* k_1Q[ii]  # <modify> We really don't need to define k_1v and k_1q, just for the clarity of the code.
 
+            #print(k_1Q)
+
             for ii in range(N_Cells):  # These are the variables at {n+1}
                 A_1[ii]   = V_1[ii] / L[ii]
                 U_1[ii]   = Q_1[ii] / A_1[ii]
                 Eta_1[ii] = A_1[ii] / B[ii] + Z[ii]
-                E_1[ii]   = ((U_1[ii])**2) /2 +  Gravity*Eta_1[ii]
-                l_P_1[ii] = B[ii] + 2 * Eta_1[ii]  # <modify>
+                E_1[ii]   = ((U_1[ii])**2.0) /2.0 +  Gravity*Eta_1[ii]
+                l_P_1[ii] = B[ii] + 2.0 * (Eta_1[ii] - Z[ii])  # <modify>
                 R_h_1[ii] = A_1[ii] / l_P_1[ii] # <modify>
-                C_1[ii]   = ((M[ii])**2) / ((R_h_1[ii])**(4.0/3.0)) # <modify>
-                F_1[ii]   = Gravity * C_1[ii] * V_1[ii] * ((U_1[ii])**2)
+                C_1[ii]   = ((M[ii])**2.0) / ((R_h_1[ii])**(4.0/3.0)) # <modify>
+                F_1[ii]   = Gravity * C_1[ii] * V_1[ii] * ((U_1[ii])**2.0)
+
+            if nn < slowness:
+              Q_Upstream = Ex.Q_Up * ((nn+0.5)/ float(slowness))
+            else:
+              Q_Upstream = Ex.Q_Up
+
+
+            # <delete>
+            if (nn%10000) == 0:
+                RealTime = round(nn*DT,5)
+                TITLE = " at half time: " + str(RealTime)
+                Draw.Plot_at_Cell(N_Cells, X, Z, Q_1, V_1, Eta_1, U_1, E_1, A_1, TITLE)
 
             for ii in range(N_Cells+1):
                 if ii==0: # Boundary condition at face 1/2
-                    Q_F_1[ii]   = Ex.Q_Up
-                    A_F_1[ii]   = A_1[ii]
-                    U_F_1[ii]   = Q_F_1[ii] / A_F_1[ii]
+                    Q_F_1[ii]   = Q_Upstream
+                    #A_F_1[ii]   = A_1[ii]
+                    #U_F_1[ii]   = Q_F_1[ii] / A_F_1[ii]
+                    #Eta_F_1[ii] = Eta_1[ii] + L[ii] * (( Z[ii] - Z[ii+1]) / (L[ii] + L[ii+1]))
+
+                    Z_0 = Z[ii] + L[ii] * (( Z[ii] - Z[ii+1]) / (L[ii] + L[ii+1]))
+                    #Eta_F[ii] = h_upstream+ Z_0
                     Eta_F_1[ii] = Eta_1[ii] + L[ii] * (( Z[ii] - Z[ii+1]) / (L[ii] + L[ii+1]))
-                    E_F_1[ii]   = ((U_F_1[ii])**2)/2 + Gravity * Eta_F_1[ii]
+                    A_F_1[ii]   =  (Eta_F_1[ii] - Z_0)* B[ii]
+
+                    U_F_1[ii]   = Q_F_1[ii] / A_F_1[ii]
+                    E_F_1[ii]   = ((U_F_1[ii])**2.0)/2.0 + Gravity * Eta_F_1[ii]
                 elif ii != 0 and ii != N_Cells: # middle cells
                     A_F_1[ii]   = (L[ii]*  A_1[ii-1] + L[ii-1]*  A_1[ii] )/( L[ii] + L[ii-1] )
                     Eta_F_1[ii] = (L[ii]*Eta_1[ii-1] + L[ii-1]*Eta_1[ii] )/( L[ii] + L[ii-1] )
+                    #if Eta_F_1[ii] != A_F_1[ii] / B[ii]  + Z_F[2*ii]:
+                    #    print(" Fatal Error in the half step: inconsistency btw A and eta: %d %d %30.25f %30.25f %f " % (nn, ii, Eta_F_1[ii], A_F_1[ii] / B[ii]  + Z_F[2*ii] , Z_F[2*ii]))
+                    #    check = input(" Error: press ENTER to exit ")
+                    #    sys.exit()
                     # Temp: E_{i+1/2}^{n+1/2}
                     E_F_1[ii]   = ( 1.0 / (V_1[ii-1]+V_1[ii]) ) * ( E_F[ii] *( V[ii-1] + V[ii] ) - E_1[ii-1]*V_1[ii-1] - E_1[ii]*V_1[ii] + E[ii-1]*V[ii-1] + E[ii]*V[ii] + DT * ( Q_1[ii-1] * E_1[ii-1] - Q_1[ii] * E_1[ii] - (1.0/2.0) * ( U_1[ii-1] * F_1[ii-1] + U_1[ii] * F_1[ii]  ) + Q[ii-1] * E[ii-1] - Q[ii] * E[ii] - (1.0/2.0) * ( U[ii-1] * F[ii-1] + U[ii] * F[ii]  )   ) )
                     if E_F_1[ii]  < 0:
@@ -259,7 +319,7 @@ class Solver:
                         print(" %30.20f" % (E_F_1[ii] - Gravity * Eta_F_1[ii]))
                         check = input(" Error: press ENTER to exit ")
                         sys.exit()
-                    U_F_1[ii]   = ( 2*(E_F_1[ii] - Gravity * Eta_F_1[ii] ) )**(0.5)
+                    U_F_1[ii]   = ( 2.0*(E_F_1[ii] - Gravity * Eta_F_1[ii] ) )**(0.5)
                     Q_F_1[ii]   = A_F_1[ii] * U_F_1[ii]          
                 elif ii == N_Cells: # Boundary condition at face N+1/2
                     Delta_Z   = L[ii-1] * ( ( Z[ii-2]-Z[ii-1] )/( L[ii-2]+L[ii-1] )  )
@@ -273,13 +333,14 @@ class Solver:
                     #Q_F_1[ii]   = (1.0/M[ii-1]) * A_F_1[ii] * ((R_h_1[ii-1])**(2.0/3.0))  * ( ( (Eta_1[ii-1]-Eta_F_1[ii])/(0.5*L[ii-1])  )**(1.0/2.0) )  # <modify>
                     Q_F_1[ii]   = Q_1[ii-1]
                     U_F_1[ii]   = Q_F_1[ii] / A_F_1[ii]
-                    E_F_1[ii]   = ((U_F_1[ii])**2)/2 + Gravity * Eta_F_1[ii]
+                    E_F_1[ii]   = ((U_F_1[ii])**2.0)/2.0 + Gravity * Eta_F_1[ii]
 
             # <delete>
-            if (nn%100) == 0:
+            if (nn%10000) == 0:
                 RealTime = round(nn*DT,5)
                 TITLE = "k-2 at time: " + str(RealTime)
                 Draw.Plot_Full(3, N_Cells, X_F, Z_F, Q_1, Q_F_1, Eta_1, Eta_F_1, U_1, U_F_1, E_1, E_F_1, A_1, A_F_1, TITLE)
+
 
             for ii in range(N_Cells): # To find k2 in the Runge-Kutta method and find the solution at n + 1
                 V_0[ii]  = V[ii]
@@ -290,17 +351,19 @@ class Solver:
                 E_F_0[ii]= E_F[ii]
 
                 k_2V[ii]  = DT * ( Q_F_1[ii] - Q_F_1[ii+1] )  # <modify> remove
-                k_2Q[ii]  = (DT / L[ii]) * ( Q_F_1[ii] * U_F_1[ii] - Q_F_1[ii+1] * U_F_1[ii+1] + Gravity * A_F_1[ii]* Eta_F_1[ii] - Gravity * A_F_1[ii+1]* Eta_F_1[ii+1] -F_1[ii] ) # <modify> remove
+                k_2Q[ii]  = (DT / L[ii]) * ( Q_F_1[ii] * U_F_1[ii] - Q_F_1[ii+1] * U_F_1[ii+1] + Gravity * A_F_1[ii]* Eta_F_1[ii] - Gravity * A_F_1[ii+1]* Eta_F_1[ii+1]  -F_1[ii] ) # <modify> remove
+
+                #print(k_2Q)
 
                 V[ii] = V[ii] + k_2V[ii]
                 Q[ii] = Q[ii] + k_2Q[ii]
 
 
-            #if (nn%10000) == 0:
-            #    RealTime = nn*DT
-            #    TITLE = " at time: " + str(RealTime)
-            #    Draw.Plot_at_Cell(N_Cells, X, Z, Q, V, Eta, U, E, A, TITLE)
         # End loop on the time steps
 
         print(" ========== Solver Class ==========")
         print()
+
+
+
+
