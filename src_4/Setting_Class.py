83848cfa7c935fe013d0120bcf43855c1b339f09
Babak Poursartip <babakp@utexas.edu>
2017-12-06 09:58:41 -0600

    Add files via upload

---
+++ src_4/Setting_Class.py
@@ -0,0 +1,32 @@
+
+
+###############################################################################
+#
+# Code developed by: Dr. Babak Poursartip
+# Supervised by:     Dr. Ben R. Hodges
+# 
+# Start date:    11/27/2017
+# Latest update: 11/27/2017
+#
+# Comment: This class defines the various setting required for this code.
+#
+###############################################################################
+
+class Setting:
+
+    def __init__(self):
+
+        self.DT_min = 1.0e-6
+        self.DT_max = 1
+
+        self.Plot_at_Cell = 5 # Plots the results at the cell center, every "Plot_at_Cell" steps.
+        self.Plot_at_Face = 10 # Plots the results at the cell center, every "Plot_at_Face" steps.
+
+        self.CFLu_max = 1.0
+        self.CFLb_max = 1.0
+
+        self.Fr_max = 10000.0
+
+        self.depth_min = 0.001
+
+        self.time_advance = 'rk4' # Options are: rk4 rk2 forward_euler
